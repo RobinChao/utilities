eb5dc2790c3a81b3975a5df98898ce00a584e327
jno <ed@ngslab.ru>
2016-08-29 11:11:18 +0300

    track-dhcp.sh: add interactive intf selection

--- track-dhcp.sh
+++ track-dhcp.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+echo "$(basename "$0") will track BOOTP/DHCP activity on the specified/selected interface.">&2
+echo >&2
+
 intf="$1" ; shift
 if [ -z "$intf" ]; then
-	echo "$(basename "$0") <interface> [<tcpdump options...>]">&2
+	echo "$(basename "$0") [-i | <interface>] [<tcpdump options...>]">&2
 	echo "Enabled interfaces:" >&2
 	ip link | grep -w 'state UP' | cut -d: -f2 | cat -n >&2
 	exit 1
+elif [ "$intf" = '-i' ]; then
+	echo "Enabled interfaces:" >&2
+	select intf in `ip link | grep -w 'state UP' | cut -d: -f2`; do
+		[ -n "$intf" ] && break
+	done
 fi
 
+log="${intf}.tcpdump"
+
+[ -f "$log" ] && { echo >> "$log"; echo '==========' >> "$log"; }
+date '+%F %T' >> "$log"
+
 set -x
-tcpdump -i "$intf" -s 1500 'port 67 or port 68' -e "$@"
+tcpdump -i "$intf" -s 1500 'port 67 or port 68' -e "$@" | tee -a -i "$log"
 # EOF #
