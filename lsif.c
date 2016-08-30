1fd178a3dcf72f75022416b0c8af0c61338b88a7
jno <ed@ngslab.ru>
2016-08-30 14:01:55 +0300

    lsif.c: add more ways to query interfaces

--- lsif.c
+++ lsif.c
@@ -3,27 +3,47 @@
  *
  * For those who have pissed off awking output of ip(8)...
  *
- * [root@opt-03 ~]# ./lsif 
+ * [root@opt-03 ~]# ./lsif
  *  1: lo              : 127.0.0.1      : LOOPBACK -
  *  2: eno1            : 172.17.32.102  : ETHER 6c:ae:8b:2c:eb:18
  *  3: br-ctlplane     : 192.0.2.1      : ETHER 6c:ae:8b:2c:eb:19
  *  4: br-ctlplane     : 192.0.2.3      : ETHER 6c:ae:8b:2c:eb:19
  *  5: br-ctlplane     : 192.0.2.2      : ETHER 6c:ae:8b:2c:eb:19
+ *
+ * See netdevice(7) for
+ * - SIOCGIFADDR
+ * - SIOCGIFBRDADDR
+ * - SIOCGIFCONF (here)
+ * - SIOCGIFDSTADDR
+ * - SIOCGIFFLAGS
+ * - SIOCGIFHWADDR (here)
+ * - SIOCGIFINDEX
+ * - SIOCGIFMAP
+ * - SIOCGIFMETRIC
+ * - SIOCGIFMTU
+ * - SIOCGIFNAME
+ * - SIOCGIFNETMASK
+ * - SIOCGIFPFLAGS
+ * - SIOCGIFTXQLEN
  */
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+#include <netdb.h>
+#include <ifaddrs.h>
+#include <linux/if_link.h>
 #include <linux/types.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
-#include <sys/ioctl.h>
 #include <net/if.h>
-#include <arpa/inet.h>
 #include <net/if_arp.h>
+#include <arpa/inet.h>
 
 /* legacy mode decls */
 extern int getopt(int argc, char * const argv[], const char *optstring);
@@ -34,10 +54,14 @@ extern int optind, opterr, optopt;
 typedef enum { false = 0, true = 1 } bool;
 
 static struct {
+	int mode;
 	bool ifname_set;
 	char ifname[IFNAMSIZ+1];
 	bool show_ip;
 	bool show_mac;
+	bool flags_set;
+	short flags;
+	bool verbose;
 } global = {};
 
 int run_ioctl(int opcode, void *arg)
@@ -45,13 +69,13 @@ int run_ioctl(int opcode, void *arg)
 	int rc, sock = socket(AF_INET, SOCK_DGRAM, 0);
 
 	if (sock < 0) {
-		perror("socket");
+		if (global.verbose) perror("socket");
 		return sock;
 	}
 
 	rc = ioctl(sock, opcode, arg);
 	if (rc) {
-		// perror("ioctl");
+		// if (global.verbose) perror("ioctl");
 		close(sock);
 		return rc;
 	}
@@ -66,7 +90,7 @@ int query_response_size(void)
 	int rc = run_ioctl(SIOCGIFCONF, &ifc);
 
 	if (rc) {
-		perror("query_response_size");
+		if (global.verbose) perror("query_response_size");
 		return rc;
 	}
 
@@ -78,7 +102,34 @@ int query_if_hwaddr(struct ifreq *req)
 	int rc = run_ioctl(SIOCGIFHWADDR, req);
 
 	if (rc) {
-		perror("query_if_hwaddr:ioctl");
+		if (global.verbose) perror("query_if_hwaddr:ioctl");
+		return rc;
+	}
+
+	return 0;
+}
+
+int query_ip_addr(struct ifreq *req)
+{
+	int rc;
+
+	req->ifr_addr.sa_family = AF_INET;
+	rc = run_ioctl(SIOCGIFADDR, req);
+
+	if (rc) {
+		if (global.verbose) perror("query_ip_addr:ioctl");
+		return rc;
+	}
+
+	return 0;
+}
+
+int query_if_flags(struct ifreq *req)
+{
+	int rc = run_ioctl(SIOCGIFHWADDR, req);
+
+	if (rc) {
+		if (global.verbose) perror("query_if_flags:ioctl");
 		return rc;
 	}
 
@@ -90,22 +141,180 @@ enum addr_fmt {
 	ADDR_FMT_SHORT,
 };
 
-static inline char *ipv4addr(const struct sockaddr *sa, enum addr_fmt fmt)
-{
-	static char addr[64], *p, *fmx;
-	int i;
+static const char iff_flag_delimiter[] = ",";
+#define _STR(x) ""#x
+#define IFF_FLAG2STR(flag, value, string, descr)			\
+	do {								\
+		if ((value) & (IFF_##flag)) {				\
+			if ((string)[0])				\
+				strcat((string), iff_flag_delimiter);	\
+			strcat((string), _STR(flag));			\
+		}							\
+	} while (0)
 
-	switch (sa->sa_family) {
+static inline char *if_flags(short flags, enum addr_fmt fmt)
+{
+	static char sflags[128];
+
+	memset(sflags, 0, sizeof(sflags));
+	/* sprintf(sflags, "0x%0*x", (int)sizeof(flags) * 2, flags); */
+	IFF_FLAG2STR(UP, flags, sflags, "Interface is up");
+	IFF_FLAG2STR(BROADCAST, flags, sflags, "Broadcast address valid");
+	IFF_FLAG2STR(DEBUG, flags, sflags, "Turn on debugging");
+	IFF_FLAG2STR(LOOPBACK, flags, sflags, "Is a loopback net");
+	IFF_FLAG2STR(POINTOPOINT, flags, sflags, "Interface is point-to-point link");
+	IFF_FLAG2STR(NOTRAILERS, flags, sflags, "Avoid use of trailers");
+	IFF_FLAG2STR(RUNNING, flags, sflags, "Resources allocated");
+	IFF_FLAG2STR(NOARP, flags, sflags, "No address resolution protocol");
+	IFF_FLAG2STR(PROMISC, flags, sflags, "Receive all packets");
+	IFF_FLAG2STR(ALLMULTI, flags, sflags, "Receive all multicast packets");
+	IFF_FLAG2STR(MASTER, flags, sflags, "Master of a load balancer");
+	IFF_FLAG2STR(SLAVE, flags, sflags, "Slave of a load balancer");
+	IFF_FLAG2STR(MULTICAST, flags, sflags, "Supports multicast");
+	IFF_FLAG2STR(PORTSEL, flags, sflags, "Can set media type");
+	IFF_FLAG2STR(AUTOMEDIA, flags, sflags, "Auto media select active");
+	IFF_FLAG2STR(DYNAMIC, flags, sflags, "Dialup device with changing addresses");
+
+	return sflags;
+}
+
+static const char *_families[] = {
+	[AF_UNSPEC] = "UNSPEC",
+	[AF_LOCAL] = "LOCAL", /* == [AF_UNIX] == [AF_FILE] */
+	[AF_INET] = "INET",
+	[AF_AX25] = "AX25",
+	[AF_IPX] = "IPX",
+	[AF_APPLETALK] = "APPLETALK",
+	[AF_NETROM] = "NETROM",
+	[AF_BRIDGE] = "BRIDGE",
+	[AF_ATMPVC] = "ATMPVC",
+	[AF_X25] = "X25",
+	[AF_INET6] = "INET6",
+	[AF_ROSE] = "ROSE",
+	[AF_DECnet] = "DECnet",
+	[AF_NETBEUI] = "NETBEUI",
+	[AF_SECURITY] = "SECURITY",
+	[AF_KEY] = "KEY",
+	[AF_NETLINK] = "NETLINK",
+	[AF_ROUTE] = "ROUTE",
+	[AF_PACKET] = "PACKET",
+	[AF_ASH] = "ASH",
+	[AF_ECONET] = "ECONET",
+	[AF_ATMSVC] = "ATMSVC",
+	[AF_RDS] = "RDS",
+	[AF_SNA] = "SNA",
+	[AF_IRDA] = "IRDA",
+	[AF_PPPOX] = "PPPOX",
+	[AF_WANPIPE] = "WANPIPE",
+	[AF_LLC] = "LLC",
+	[AF_IB] = "IB",
+	[AF_MPLS] = "MPLS",
+	[AF_CAN] = "CAN",
+	[AF_TIPC] = "TIPC",
+	[AF_BLUETOOTH] = "BLUETOOTH",
+	[AF_IUCV] = "IUCV",
+	[AF_RXRPC] = "RXRPC",
+	[AF_ISDN] = "ISDN",
+	[AF_PHONET] = "PHONET",
+	[AF_IEEE802154] = "IEEE802154",
+	[AF_CAIF] = "CAIF",
+	[AF_ALG] = "ALG",
+	[AF_NFC] = "NFC",
+	[AF_VSOCK] = "VSOCK",
+};
+
+static inline const char *af_name(int af)
+{
+	if (af >= 0 && af < AF_MAX)
+		return _families[af];
+	return NULL;
+}
+
+static inline char *add_af_name(char *s, int af, enum addr_fmt fmt)
+{
+	static int af_name_len_max;
+
+	if (!af_name_len_max) {
+		int i;
+
+		for (i = 0; i < AF_MAX; i++) {
+			int l = strlen(af_name(i));
+
+			if (l > af_name_len_max)
+				af_name_len_max = l;
+		}
+		af_name_len_max++; /* add a space */
+	}
+
+	switch (fmt) {
+	case ADDR_FMT_LONG:
+		sprintf(s, "%-*.*s", af_name_len_max, af_name_len_max, af_name(af));
+		break;
+	case ADDR_FMT_SHORT:
+		strcpy(s, af_name(af));
+		strcat(s, " ");
+		break;
+	}
+	s += strlen(s);
+	return s;
+}
+
+#define min(x,y) (((x) > (y)) ? (y) : (x))
+
+static inline void rpad4fmt(char *s, int s_size, int pad_size, enum addr_fmt fmt)
+{
+	while (fmt == ADDR_FMT_LONG && strlen(s) < min(s_size, pad_size))
+		strncat(s, " ", s_size);
+}
+
+static inline char *ip_addr(const struct sockaddr *sa, enum addr_fmt fmt)
+{
+	const int af = sa->sa_family;
+	static char addr[64];
+	char *fmx, *p;
+	int i, rc, pad_size, inf_size;
+
+	p = add_af_name(addr, af, fmt);
+
+	switch (af) {
+	case AF_INET:  pad_size = strlen(addr) + 15; inf_size = sizeof(struct sockaddr_in); break;
+	case AF_INET6: pad_size = strlen(addr) + 40; inf_size = sizeof(struct sockaddr_in6); break;
+	default: pad_size = 0; inf_size = -1; break;
+	}
+
+	switch (af) {
+	case AF_INET:
+	case AF_INET6:
+		rc = getnameinfo(sa, inf_size, p, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+		if (rc) {
+			strcat(addr, "<error:");
+			strncat(addr, gai_strerror(rc), sizeof(addr));
+			strncat(addr, ">", sizeof(addr));
+			return addr;
+		}
+		rpad4fmt(addr, sizeof(addr), pad_size, fmt);
+		break;
+		/*
 	case AF_INET:
 		switch (fmt) {
 		case ADDR_FMT_LONG:	fmx = "%-15.15s"; break;
 		case ADDR_FMT_SHORT:	fmx = "%s"; break;
 		}
-		// sprintf(addr, "INET %-15.15s", 
-		sprintf(addr, fmx, inet_ntoa(*(struct in_addr *)&sa->sa_data[2]));
+		switch (global.mode) {
+		case 1: p = addr; break;
+		case 2:
+		case 3:
+			break;
+		default: assert(false); break;
+		}
+		sprintf(p, fmx, inet_ntoa(*(struct in_addr *)&sa->sa_data[2]));
+		break;
+		*/
+	case AF_PACKET: /* no real IP here, of course */
+		strcat(p, "-");
 		break;
 	default:
-		sprintf(addr, "<%d/", sa->sa_family);
+		sprintf(p, "<%d/", af);
 		for (i = 0, p = addr + strlen(addr); i < sizeof(sa->sa_data); i++)
 			sprintf(p + i * 3, "%02x%s", (uint8_t) sa->sa_data[i],
 				(i < sizeof(sa->sa_data) - 1) ? ":" : ">");
@@ -116,10 +325,14 @@ static inline char *ipv4addr(const struct sockaddr *sa, enum addr_fmt fmt)
 
 static inline char *hw_addr(const struct sockaddr *sa, enum addr_fmt fmt)
 {
-	static char *p, *fmx, mac[64]; /* ETHER aa:bb:cc:dd:ee:ff */
+	const int af = sa->sa_family;
+	uint8_t *data = (uint8_t *) &sa->sa_data[0];
+	const size_t data_size = sizeof(sa->sa_data);
+	static char mac[64]; /* ETHER aa:bb:cc:dd:ee:ff */
+	char *p, *fmx;
 	int i;
 
-	switch (sa->sa_family) {
+	switch (af) {
 	case ARPHRD_LOOPBACK:
 		switch (fmt) {
 		case ADDR_FMT_LONG:	fmx = "LOOPBACK -"; break;
@@ -132,41 +345,155 @@ static inline char *hw_addr(const struct sockaddr *sa, enum addr_fmt fmt)
 		case ADDR_FMT_LONG:	fmx = "ETHER %02x:%02x:%02x:%02x:%02x:%02x"; break;
 		case ADDR_FMT_SHORT:	fmx = "%02x:%02x:%02x:%02x:%02x:%02x"; break;
 		}
-		sprintf(mac, fmx,
-			(uint8_t) sa->sa_data[0], (uint8_t) sa->sa_data[1],
-			(uint8_t) sa->sa_data[2], (uint8_t) sa->sa_data[3],
-			(uint8_t) sa->sa_data[4], (uint8_t) sa->sa_data[5]);
+		sprintf(mac, fmx, data[0], data[1], data[2], data[3], data[4], data[5]);
 		break;
 	default:
 		switch (fmt) {
 		case ADDR_FMT_LONG:
-			sprintf(mac, "<%d> <", sa->sa_family);
+			sprintf(mac, "<%d> <", af);
 			break;
 		case ADDR_FMT_SHORT:
 			strcpy(mac, "<");
 			break;
 		}
-		for (i = 0, p = mac + strlen(mac); i < sizeof(sa->sa_data); i++)
-			sprintf(p + i * 3, "%02x%s", (uint8_t) sa->sa_data[i],
-				(i < sizeof(sa->sa_data) - 1) ? ":" : ">");
+		for (i = 0, p = mac + strlen(mac); i < data_size; i++)
+			sprintf(p + i * 3, "%02x%s", data[i],
+				(i < data_size - 1) ? ":" : ">");
 		break;
 	}
 
 	return mac;
 }
 
-int query_if_list(void)
+static inline void add_ip_addr(void *ifx, const char *errmsg, enum addr_fmt fmt)
 {
-	struct ifreq *req = NULL;
+	struct sockaddr *sa;
+	struct ifreq *ifr = ifx;
+	struct ifaddrs *ifa = ifx;
+	char *out;
+	int rc = 0;
+
+	switch (global.mode) {
+	case 1: /* already fetched */ sa = &ifr->ifr_addr; break;
+	case 2: rc = query_ip_addr(ifr); sa = &ifr->ifr_addr; break;
+	case 3: /* already fetched */ sa = ifa->ifa_addr; break;
+	}
+
+	fputs(rc ? errmsg : ip_addr(sa, fmt), stdout);
+}
+
+static inline void add_hw_addr(struct ifreq *ifr, const char *errmsg, enum addr_fmt fmt)
+{
+	fputs(query_if_hwaddr(ifr) ? errmsg : hw_addr(&ifr->ifr_addr, fmt), stdout);
+}
+
+static inline void add_flags(void *ifx, const char *errmsg, enum addr_fmt fmt)
+{
+	struct ifreq *ifr = ifx;
+	struct ifaddrs *ifa = ifx;
+	int flags = -1;
+
+	switch (global.mode) {
+	case 1:
+	case 2: flags = query_if_flags(ifr) ? -1 : ifr->ifr_flags; break;
+	case 3: /* already fetched */ flags = ifa->ifa_flags; break;
+	}
+
+	fputs((flags == -1) ? errmsg : if_flags(flags, fmt), stdout);
+}
+
+static inline void add_if_name(void *ifx, enum addr_fmt fmt)
+{
+	struct ifreq *ifr = ifx;
+	struct ifaddrs *ifa = ifx;
+	char *name;
+
+	switch (global.mode) {
+	case 1:
+	case 2: name = ifr->ifr_name; break;
+	case 3: name = ifa->ifa_name; break;
+	}
+
+	switch (fmt) {
+	case ADDR_FMT_LONG:
+		printf("%-*.*s", IFNAMSIZ, IFNAMSIZ, name);
+		break;
+	case ADDR_FMT_SHORT:
+		fputs(name, stdout);
+		break;
+	}
+}
+
+static void print_interface(struct ifreq *ifr)
+{
+	if (global.ifname_set) {
+		if (strncmp(ifr->ifr_name, global.ifname, IFNAMSIZ))
+			return;
+		if (global.show_ip || !global.show_mac) {
+			add_ip_addr(ifr, NULL, ADDR_FMT_SHORT);
+			fputs(global.show_ip ? "" : "\t", stdout);
+		}
+		if (global.show_mac || !global.show_ip)
+			add_hw_addr(ifr, "<no-hw-addr>", ADDR_FMT_SHORT);
+		putchar('\n');
+		return;
+	}
+
+	add_if_name(ifr, ADDR_FMT_LONG);
+	printf(" : ");
+	add_ip_addr(ifr, "<error> -", ADDR_FMT_LONG);
+	printf(" : ");
+	add_hw_addr(ifr, "<error> <no-hw-addr>", ADDR_FMT_LONG);
+
+	if (global.flags_set) {
+		printf(" : ");
+		add_flags(ifr, "<error-no-flags>", ADDR_FMT_LONG);
+	}
+
+	putchar('\n');
+}
+
+static void print_address(struct ifaddrs *ifa)
+{
+	if (global.ifname_set) {
+		if (strncmp(ifa->ifa_name, global.ifname, IFNAMSIZ))
+			return;
+	} else {
+		add_if_name(ifa, ADDR_FMT_LONG);
+		printf(" : ");
+	}
+	if (ifa->ifa_addr == NULL) {
+		printf("<no-address>\n");
+		return;
+	}
+	add_ip_addr(ifa, "<error> -", ADDR_FMT_LONG);
+	if (global.flags_set) {
+		printf(" : ");
+		add_flags(ifa, "<error-no-flags>", ADDR_FMT_LONG);
+	}
+	putchar('\n');
+	return;
+}
+
+static void __dispose_req(struct ifreq **req)
+{
+	if (req && *req) {
+		free(*req);
+		*req = 0;
+	}
+}
+
+static int __1__query_if_list(void)
+{
+	struct ifreq *req __attribute__((cleanup(__dispose_req))) = NULL;
 	struct ifconf ifc = {};
-	char ifnamefmt[16];
 	int rc, i, j, size = query_response_size();
 
 	if (size <= 0) return -1;
 
 	req = calloc(size, 1);
 	if (!req) {
-		perror("query_if_list:cmalloc");
+		if (global.verbose) perror("query_if_list:cmalloc");
 		return -1;
 	}
 
@@ -175,55 +502,105 @@ int query_if_list(void)
 
 	rc = run_ioctl(SIOCGIFCONF, &ifc);
 	if (rc) {
-		perror("query_if_list:ioctl");
+		if (global.verbose) perror("query_if_list:ioctl");
 		return rc;
 	}
 
-	sprintf(ifnamefmt, "%%-%d.%ds", IFNAMSIZ, IFNAMSIZ);
 	for (i = 0; i < ifc.ifc_len / sizeof(struct ifreq); i++) {
 		struct ifreq *ifr = &ifc.ifc_req[i];
 
 		if (global.ifname_set) {
 			if (strncmp(ifr->ifr_name, global.ifname, IFNAMSIZ))
 				continue;
-
-			if (global.show_ip || !global.show_mac)
-				printf("%s%s", ipv4addr(&ifr->ifr_addr, ADDR_FMT_SHORT),
-					global.show_ip ? "" : "\t");
-			if (global.show_mac || !global.show_ip)
-				printf("%s", query_if_hwaddr(ifr)
-					? "<no-hw-addr>"
-					: hw_addr(&ifr->ifr_addr, ADDR_FMT_SHORT));
-			putchar('\n');
-		} else {
+		} else
 			printf("%2d: ", i + 1);
-			printf(ifnamefmt, ifr->ifr_name);
-			printf(": %-15s", ipv4addr(&ifr->ifr_addr, ADDR_FMT_LONG));
-
-			if (query_if_hwaddr(ifr))
-				printf("<error> <no-hw-addr>");
-			else {
-				printf(": %s", hw_addr(&ifr->ifr_addr, ADDR_FMT_LONG));
-			}
-
-			printf("\n");
-		}
+		print_interface(ifr);
 	}
 
 	return 0;
 }
 
-static char HELP[] = "%s [-i <interface> [-a | -m]]\n\n"
+static void __dispose_if_list_2(struct if_nameindex **if_list)
+{
+	if (if_list && *if_list) {
+		if_freenameindex(*if_list);
+		*if_list = 0;
+	}
+}
+
+static int __2__query_if_list(void)
+{
+	struct if_nameindex *i;
+	struct if_nameindex *if_list
+		__attribute__((cleanup(__dispose_if_list_2)))
+		= if_nameindex();
+
+	if (!if_list) {
+		if (global.verbose) perror("if_nameindex");
+		return -1;
+	}
+	for (i = if_list; ! (i->if_index == 0 && i->if_name == NULL); i++) {
+		struct ifreq ifr;
+
+		// printf("%u: %s\n", i->if_index, i->if_name);
+		strncpy(ifr.ifr_name, i->if_name, sizeof(ifr.ifr_name));
+		print_interface(&ifr);
+	}
+
+	return 0;
+}
+
+static void __dispose_if_list_3(struct ifaddrs **if_list)
+{
+	if (if_list && *if_list) {
+		freeifaddrs(*if_list);
+		*if_list = 0;
+	}
+}
+
+static int __3__query_if_list(void)
+{
+	struct ifaddrs *ifa;
+	struct ifaddrs *if_list __attribute__((cleanup(__dispose_if_list_3))) = NULL;
+
+	if (getifaddrs(&if_list) == -1) {
+		if (global.verbose) perror("getifaddrs");
+		return -1;
+	}
+
+	for (ifa = if_list; ifa != NULL; ifa = ifa->ifa_next) {
+//		if (!ifa->ifa_addr)
+//			continue;
+		print_address(ifa);
+	}
+}
+
+int query_if_list(void)
+{
+	switch (global.mode) {
+	case 1: return __1__query_if_list();
+	case 2: return __2__query_if_list();
+	case 3: return __3__query_if_list();
+	}
+}
+
+static char HELP[] = "%s [-f] [-i <interface> [-a | -m]]\n\n"
 	"Options:\n"
 	" -h -- this help\n"
+	" -1, -2, -3 -- use different query schemes:\n"
+	"	SIOCGIFCONF, if_nameindex, getifaddrs\n"
 	" -i <interface> -- set <interface> name to query for\n"
 	" -a -- list IP addresses\n"
 	" -m -- list MAC addresses (you may want to use 'sort -u') here\n"
+	" -f -- add flags to the output\n"
+	" -v -- verbose\n"
 	"\n";
 
 static int parse_args(int argc, char * const argv[])
 {
-	const static char optspec[] = "hi:am";
+	const static char optspec[] = "hvi:amf123";
+
+	global.mode = 1; /* default */
 
 	while (true) {
 		int opt = getopt(argc, argv, optspec);
@@ -235,6 +612,17 @@ static int parse_args(int argc, char * const argv[])
 		case 'h':
 			printf(HELP, argv[0]);
 			exit(EXIT_SUCCESS);
+
+		case '1': global.mode = 1; break;
+		case '2': global.mode = 2; break;
+		case '3': global.mode = 3; break;
+		case '4': global.mode = 4; break;
+		case '5': global.mode = 5; break;
+		case '6': global.mode = 6; break;
+		case '7': global.mode = 7; break;
+		case '8': global.mode = 8; break;
+		case '9': global.mode = 9; break;
+
 		case 'i':
 			global.ifname_set = true;
 			strncpy(global.ifname, optarg, IFNAMSIZ);
@@ -244,6 +632,12 @@ static int parse_args(int argc, char * const argv[])
 			continue;
 		case 'm':
 			global.show_mac = true;
+			continue;
+		case 'f':
+			global.flags_set = true;
+			continue;
+		case 'v':
+			global.verbose = true;
 			continue;
 		default:
 			return -1;
